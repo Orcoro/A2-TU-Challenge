d3f93e49ec62827813d63f5b56f5571753c7a488
Orcoro <45428006+Orcoro@users.noreply.github.com>
2023-01-04 18:02:20 +0100

    oui oui oui fast

--- TU Challenge/MyMathImplementation.cs
+++ TU Challenge/MyMathImplementation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,116 @@ namespace TU_Challenge
 {
     public class MyMathImplementation
     {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static bool IsDivisible(int a, int b)
+        {
+            return a % b == 0;
+        }
+
+        public static bool IsEven(int a)
+        {
+            return a % 2 == 0;
+        }
+
+        public static bool IsMajeur(int age)
+        {
+            if (age < 0 || age > 140)
+                throw new ArgumentException();
+            if (age < 18 && age > 0)
+                return false;
+            return age >= 18;
+        }
+
+        public static bool IsPrimary(int a)
+        {
+            if (a < 2)
+                return false;
+            for (int i = 2; i < a; i++)
+                if (IsDivisible(a, i))
+                    return false;
+            return true;
+        }
+        public static List<int> GetAllPrimary(int a)
+        {
+            List<int> result = new List<int>();
+
+            for (int i = 2; i <= a; i++)
+                if (IsPrimary(i))
+                    result.Add(i);
+            return result;
+        }
+
+        public static int Power2(int a)
+        {
+            return a * a;
+        }
+
+        public static int Power(int a, int b)
+        {
+            int result = 1;
+
+            for (int i = 0; i < b; i++)
+                result *= a;
+            return result;
+        }
+
+        public static int IsInOrder(int a, int b)
+        {
+            if (a < b)
+                return 1;
+            else if (a > b)
+                return -1;
+            return 0;
+        }
+
+        public static int IsInOrderDesc(int a, int b)
+        {
+            return -IsInOrder(a, b);
+        }
         
+        public static bool IsListInOrder(List<int> list)
+        {
+            for (int i = 0; i < list.Count - 1; i++)
+                if (list[i] > list[i + 1])
+                    return false;
+            return true;
+        }
+
+        public static bool IsListInOrderDesc(List<int> list)
+        {
+            for (int i = 0; i < list.Count - 1; i++)
+                if (list[i] < list[i + 1])
+                    return false;
+            return true;
+        }
+
+        public static List<int> Sort(List<int> toSort)
+        {
+            while (!IsListInOrder(toSort))
+                for (int i = 0; i < toSort.Count - 1; i++)
+                    if (toSort[i] > toSort[i + 1]) {
+                        int temp = toSort[i];
+                        toSort[i] = toSort[i + 1];
+                        toSort[i + 1] = temp;
+                    }
+            return toSort;
+        }
+
+        public static List<int> GenericSort(List<int> toSort, Func<int, int, int> Order)
+        {
+            while (!IsListInOrder(toSort))
+                for (int i = 0; i < toSort.Count - 1; i++)
+                    if (toSort[i] > toSort[i + 1])
+                    {
+                        int temp = toSort[i];
+                        toSort[i] = toSort[i + 1];
+                        toSort[i + 1] = temp;
+                    }
+            return toSort;
+        }
     }
 }
--- TU Challenge/MyStringImplementation.cs
+++ TU Challenge/MyStringImplementation.cs
@@ -8,6 +8,102 @@ namespace TU_Challenge
 {
     public class MyStringImplementation
     {
-        
+        public static string BazardString(string input)
+        {
+            string result = "";
+            
+            for (int i = 0; i != input.Length + 1; i += 2) {
+                if (i == input.Length)
+                    i = 1;
+                result += input[i];
+            }
+            return result;
+        }
+
+        public static bool IsNullEmptyOrWhiteSpace(string input)
+        {
+            if (input == null || input == "")
+                return true;
+            for (int i = 0; i < input.Length; i++)
+                if (input[i] != ' ')
+                    return false;
+            return true;
+        }
+
+        public static string MixString(string a, string b)
+        {
+            string result = "";
+
+            if (IsNullEmptyOrWhiteSpace(a) || IsNullEmptyOrWhiteSpace(b))
+                throw new ArgumentException();
+            for (int i = 0; i < a.Length || i < b.Length; i++) {
+                if (i < a.Length)
+                    result += a[i];
+                if (i < b.Length)
+                    result += b[i];
+            }
+            return result;
+        }
+
+        public static string Reverse(string a)
+        {
+            string result = "";
+            for (int i = a.Length - 1; i >= 0; i--)
+                result += a[i];
+            return result;
+        }
+
+        public static string ToCesarCode(string input, int offset)
+        {
+            string result = "";
+
+            for (int i = 0; i < input.Length; i++) {
+                if (input[i] >= 'a' && input[i] <= 'z')
+                    result += (char)('a' + (input[i] - 'a' + offset) % 26);
+                else if (input[i] >= 'A' && input[i] <= 'Z')
+                    result += (char)('A' + (input[i] - 'A' + offset) % 26);
+                else
+                    result += input[i];
+            }
+            return result;
+        }
+
+        public static string ToLowerCase(string a)
+        {
+            string result = "";
+            
+            for (int i = 0; i < a.Length; i++) {
+                if (a[i] >= 'A' && a[i] <= 'Z' || a[i] >= 'À' && a[i] <= 'Þ')
+                    result += (char)(a[i] + 32);
+                else
+                    result += a[i];
+            }
+            return result;
+        }
+
+        public static string UnBazardString(string input)
+        {
+            string a = "";
+            string b = "";
+
+            for (int i = 0; i < input.Length / 2; i++)
+                a += input[i];
+            for (int i = input.Length / 2; i < input.Length; i++)
+                b += input[i];
+
+            return MixString(a, b);
+        }
+
+        public static string Voyelles(string a)
+        {
+            string result = "";
+
+            for (int i = 0; i < a.Length; i++)
+                if (a[i] == 'a' || a[i] == 'e' || a[i] == 'i' || a[i] == 'o' || a[i] == 'u' || a[i] == 'y' || a[i] == 'A' || a[i] == 'E' || a[i] == 'I' || a[i] == 'O' || a[i] == 'U' || a[i] == 'Y') 
+                    if (!result.Contains(a[i]))
+                        result += a[i];
+            result = ToLowerCase(result);
+            return result;
+        }
     }
 }
