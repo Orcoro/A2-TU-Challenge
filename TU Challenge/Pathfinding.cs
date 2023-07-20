6278b265659763abb12a6867e378c5e2fd9221b0
Orcoro <45428006+Orcoro@users.noreply.github.com>
2023-07-20 14:05:31 +0200

    beginning of Path Finding

--- TU Challenge/Pathfinding.cs
+++ TU Challenge/Pathfinding.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,23 @@ namespace TU_Challenge
 {
     public class Pathfinding
     {
+        private string[] _grid;
+
+        public string[] Grid {
+            get { return _grid; }
+            set { _grid = value; }
+        }
         
+        public Pathfinding(string grid)
+        {
+            int line = 0;
+
+            this._grid = new string[grid.Split('\n').Length];
+            foreach (var item in grid.Split('\n')){
+                this._grid[line] = item;
+                line++;
+            }
+        }
     }
 
 }
